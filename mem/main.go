c279438a59ef1e28bedbd86d93aca0ab3229a92a
Stefan Stjepic <stefan.stjepic03@gmail.com>
2023-10-30 15:19:57 +0100

    struktura Memtable

--- mem/main.go
+++ mem/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"mem/memtable"
+)
 
 func main() {
-	fmt.Print("Cao")
+	m := memtable.CreateMemtable(5)
+	m.AddElement("k1", []byte("aaa"))
+	_, data := m.GetElement("k1")
+	fmt.Printf("%s", data)
+	m.DeleteElement("k1")
+	_, data = m.GetElement("k1")
+	fmt.Printf("%s", data)
 }
--- mem/memtable/memtable.go
+++ mem/memtable/memtable.go
@@ -12,3 +12,51 @@ func CreateMemtable(cap int) *Memtable {
 		length:   0,
 	}
 }
+
+// funkcija koja ce se implementirati kasnije a sluzi da prosledi podatke iz memtable u SSTable
+// i da isprazni memtable kad se podaci posalju
+func (mem *Memtable) SendToSSTable() bool {
+
+	//.......
+	//.......
+	mem.data = make(map[string][]byte)
+	mem.length = 0
+	return true
+}
+
+func (mem *Memtable) AddElement(key string, data []byte) bool {
+	//ukoliko ima mesta u memtable, samo se upisuje podatak
+	if mem.length < mem.capacity {
+		mem.data[key] = data
+		mem.length++
+		return true
+
+		//neophodno je isprazniti memtable
+	} else if mem.length == mem.capacity {
+		if mem.SendToSSTable() {
+			mem.data[key] = data
+			mem.length++
+			return true
+		}
+	}
+	//ukoliko se nesto nije izvrsilo kako treba, vraca se false
+	return false
+}
+
+func (mem *Memtable) GetElement(key string) (bool, []byte) {
+	elem, err := mem.data[key]
+	if !err {
+		return false, nil
+	}
+	return true, elem
+}
+
+func (mem *Memtable) DeleteElement(key string) bool {
+	exist, _ := mem.GetElement(key)
+
+	if exist {
+		delete(mem.data, key)
+		return true
+	}
+	return false
+}
