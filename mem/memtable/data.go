a972857415eb5e4b44fd6e6bb212f98b37a9bb6f
Stefan Stjepic <stefan.stjepic03@gmail.com>
2023-11-04 18:14:03 +0100

    [ADD] changeTime atribut

--- mem/memtable/data.go
+++ mem/memtable/data.go
@@ -1,23 +1,29 @@
 package memtable
 
+import "time"
+
 type DataType struct {
-	data   []byte
-	delete byte
+	data       []byte
+	delete     byte
+	changeTime time.Time
 }
 
 func CreateDataType(data []byte) *DataType {
 	return &DataType{
-		data:   data,
-		delete: 0x00,
+		data:       data,
+		delete:     0x00,
+		changeTime: time.Now(),
 	}
 }
 
 func (dt *DataType) UpdateDataType(data []byte) {
 	dt.data = data
+	dt.changeTime = time.Now()
 }
 
 func (dt *DataType) DeleteDataType() {
 	dt.delete = 0x01
+	dt.changeTime = time.Now()
 }
 
 func (dt *DataType) IsDeleted() bool {
