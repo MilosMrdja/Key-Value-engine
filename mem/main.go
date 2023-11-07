eb4f58a672befd9df740575060d3329c539e65c2
Stefan Stjepic <stefan.stjepic03@gmail.com>
2023-11-07 10:55:40 +0100

    [ADD] memtable interface, HashMemtable struct

--- mem/main.go
+++ mem/main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
-	m := memtable.CreateMemtable(5)
 
+	i, j := memtable.LoadConfig("config.txt")
+	var m memtable.Memtable
+	if i == "hash" {
+		m = memtable.CreateHashMemtable(j)
+	} else {
+		m = memtable.CreateHashMemtable(j)
+	}
 	m.AddElement("k1", []byte("aaa"))
 	_, data := m.GetElement("k1")
 	fmt.Printf("%s \n", data)
--- mem/memtable/memtable.go
+++ mem/memtable/memtable.go
@@ -1,62 +1,37 @@
 package memtable
 
-type Memtable struct {
-	data             map[string]*DataType
-	capacity, length int
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Memtable interface {
+	AddElement(key string, data []byte) bool
+	GetElement(key string) (bool, []byte)
+	DeleteElement(key string) bool
+	SendToSSTable() bool
 }
 
-func CreateMemtable(cap int) *Memtable {
-	return &Memtable{
-		data:     make(map[string]*DataType),
-		capacity: cap,
-		length:   0,
+// funkcija koja cita podatke iz config fajla i vraca tip memtable i nje podrazumevani kapacitet
+func LoadConfig(filePath string) (string, int) {
+	file, err := os.OpenFile(filePath, os.O_RDONLY, 0666)
+	if err != nil {
+		panic(err)
 	}
-}
-
-// funkcija koja ce se implementirati kasnije a sluzi da prosledi podatke iz memtable u SSTable
-// i da isprazni memtable kad se podaci posalju
-func (mem *Memtable) SendToSSTable() bool {
-
-	//.......
-	//.......
-	mem.data = make(map[string]*DataType)
-	mem.length = 0
-	return true
-}
-
-func (mem *Memtable) AddElement(key string, data []byte) bool {
-	//ukoliko ima mesta u memtable, samo se upisuje podatak
-	if mem.length < mem.capacity {
-		e := CreateDataType(data)
-		mem.data[key] = e
-		mem.length++
-		return true
-
-		//neophodno je isprazniti memtable
-	} else if mem.length == mem.capacity {
-		if mem.SendToSSTable() {
-			mem.data[key] = CreateDataType(data)
-			mem.length++
-			return true
-		}
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	success := scanner.Scan()
+	if success == false {
+		err = scanner.Err()
+		log.Fatal(err)
 	}
-	//ukoliko se nesto nije izvrsilo kako treba, vraca se false
-	return false
-}
+	memType := scanner.Text()
+	memType = strings.Split(memType, " ")[1]
+	scanner.Scan()
 
-func (mem *Memtable) GetElement(key string) (bool, []byte) {
-	elem, err := mem.data[key]
-	if !err || elem.IsDeleted() {
-		return false, nil
-	}
-	return true, elem.data
-}
-
-func (mem *Memtable) DeleteElement(key string) bool {
-	elem, found := mem.data[key]
-	if found {
-		elem.DeleteDataType()
-		return true
-	}
-	return false
+	memCap, _ := strconv.Atoi(strings.Split(scanner.Text(), " ")[1])
+	return memType, memCap
 }
